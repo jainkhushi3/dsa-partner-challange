175677513038e377ed8211d11b781b1192c7c733
Khushi Jain <khushijain.professional@gmail.com>
2026-04-06 22:49:53 +0530

    Add a specific file

---
+++ day3.java
@@ -0,0 +1,104 @@
+import java.util.*;
+
+public class day3{
+    public static void main(String args[]){
+        // 1. Area of Circle
+        // Scanner sc = new Scanner(System.in);
+        // double pie = 3.14;
+        // System.out.print("Enter Radius : ");
+        // float rad = sc.nextFloat();
+        // double area = pie * rad * rad;
+        // System.out.print("Area of Circle is " +area);
+        // sc.close();
+
+        // 2. Area of Triangle
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter Base : ");
+        // float Base = sc.nextFloat();
+        // System.out.print("Enter Height : ");
+        // float Height = sc.nextFloat();
+        // double areaOfTriangle = 0.5 * Base * Height;
+        // System.out.print("Area of Triangle is " +areaOfTriangle);
+        // sc.close();
+
+        // 3. Area of Rectangle
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter Length : ");
+        // float Len = sc.nextFloat();
+        // System.out.print("Enter Breadth : ");
+        // float Brd = sc.nextFloat();
+        // double areaOfRec = Len * Brd;
+        // System.out.print("Area of Rectangle is " +areaOfRec);
+        // sc.close();
+
+         // 4. Area of Isosceles Triangle
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter Base : ");
+        // float Base = sc.nextFloat();
+        // System.out.print("Enter Height : ");
+        // float Height = sc.nextFloat();
+        // double areaOfTriangle = 0.5 * Base * Height;
+        // System.out.print("Area of Isosceles Triangle is " +areaOfTriangle);
+        // sc.close();
+
+        // 5. Area of Parallelogram
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter Base : ");
+        // float Base = sc.nextFloat();
+        // System.out.print("Enter Height : ");
+        // float Height = sc.nextFloat();
+        // double areaOfPar = Base * Height;
+        // System.out.print("Area of Parallelogram is " +areaOfPar);
+        // sc.close();
+
+        // 6. Area of Rhombus
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter D1 : ");
+        // float D1 = sc.nextFloat();
+        // System.out.print("Enter D2 : ");
+        // float D2 = sc.nextFloat();
+        // double areaOfRhombus = 0.5 * D1 * D2;
+        // System.out.print("Area of Rhombus is " +areaOfRhombus);
+        // sc.close();
+
+        // 7. Area of Equilateral Triangle
+        // Scanner sc = new Scanner(System.in);
+        // double underroot3 = Math.sqrt(3);
+        // System.out.print("Enter Side : ");
+        // float Side = sc.nextFloat();
+        
+        // double areaOfEqT = ( underroot3 / 4 ) * Side * Side;
+        // System.out.print("Area of Equilateral Triangle is " +areaOfEqT);
+        // sc.close();
+
+        // 8. Perimeter of Circle (Circumference)
+        // Scanner sc = new Scanner(System.in);
+        // double Pie = 3.14;
+        // System.out.print("Enter Radius : ");
+        // float Rad = sc.nextFloat();
+        
+        // double perOfCircle = 2 * Pie * Rad;
+        // System.out.print("Perimeter of Circle is " +perOfCircle);
+        // sc.close();
+
+        // 9. Perimeter of Equilateral Triangle
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter Side : ");
+        // float Side = sc.nextFloat();
+        
+        // double perOfEquT = 3 * Side;
+        // System.out.print("Perimeter of Equilateral Triangle is " +perOfEquT);
+        // sc.close();
+
+        // 10. Perimeter of Parallelogram
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter SideA : ");
+        // float a = sc.nextFloat();
+        // System.out.print("Enter SideB : ");
+        // float b = sc.nextFloat();
+        
+        // double perOfPara = 2 * (a + b);
+        // System.out.print("Perimeter of Parallelogram is " +perOfPara);
+        // sc.close();
+    }
+}
